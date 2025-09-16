20f7534d2c4113dd44907d4f773b10e67c71bcd0
vnatar1x <40783335+vnatar1x@users.noreply.github.com>
2025-09-16 09:32:43 -0600

    Add files via upload

---
+++ BankAccountApi_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankAccountApi_UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankAccountApi_UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1ed7de41-670d-44d8-843f-d24c3fa3f2a2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BankAccountApi_UnitTests/Providers/CustomerProviderTest.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using AutoMapper;
+using System.Collections.Generic;
+using BanAccountApi_Models.DataModels;
+using BanAccountApi_Models.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountApi_DataAccessLayer.DataAccess.Interfaces;
+using BankAccountApi_BusinessLayer.Providers.Implementations;
+
+namespace BankAccountApi_UnitTests.Providers
+{
+    [TestClass]
+    public class CustomerProviderTest
+    {
+        [TestMethod]
+        public void GetCustomers_Success()
+        {
+            //Setup
+            Mock<ICustomerRepo> mckCustomerRepo = new Mock<ICustomerRepo>();
+            CustomerDataModel customerDataModel = new CustomerDataModel { CustomerId = 5001, AccountId = 1001 };
+            mckCustomerRepo.Setup(x => x.GetCustomers()).Returns(new List<CustomerDataModel> { customerDataModel });
+
+            Mock<IMapper> mckMapper = new Mock<IMapper>();
+            mckMapper.Setup(x => x.Map<List<CustomerViewModel>>(new List<CustomerDataModel>())).Returns(new List<CustomerViewModel>());
+
+            CustomerProvider researchProvider = new CustomerProvider(mckCustomerRepo.Object, mckMapper.Object);
+            List<CustomerViewModel> lstCustomers = researchProvider.GetCustomers();
+
+            //Assert
+            Assert.IsInstanceOfType(lstCustomers, typeof(List<CustomerViewModel>));
+        }
+    }
+}
